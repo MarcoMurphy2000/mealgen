8c8723cde07e339f644a2f8dbd061663355b330b
Marco <MvanderMerwe@dvtsoftware.com>
2025-01-10 11:15:31 +0200

    Notification being sent to SNS topic when a new meal is generated which includes the meal name.

--- src/main/java/com/dvtsoftware/mealgen/service/impl/MealServiceImpl.java
+++ src/main/java/com/dvtsoftware/mealgen/service/impl/MealServiceImpl.java
@@ -14,6 +14,8 @@ import com.dvtsoftware.mealgen.repository.MealRepository;
 import com.dvtsoftware.mealgen.service.interfaces.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 
 @Service
 public class MealServiceImpl implements MealService {
@@ -23,25 +25,32 @@ public class MealServiceImpl implements MealService {
     private final MealRepository mealRepository;
     private final MealMapper mealMapper;
     private final MealGenerationService mealGenerationService;
+    private final SnsClient snsClient;
 
     @Autowired
     public MealServiceImpl(MealRepository mealRepository,
                            MealMapper mealMapper,
-                           MealGenerationService mealGenerationService) {
+                           MealGenerationService mealGenerationService,
+                           SnsClient snsClient) {
         this.mealRepository = mealRepository;
         this.mealMapper = mealMapper;
         this.mealGenerationService = mealGenerationService;
+        this.snsClient = snsClient;
     }
 
+    @Override
     public MealDomainObject generateMeal(MealRequestDomainObject mealRequestDomainObject) throws IOException {
         MealDomainObject mealDomainObject = mealGenerationService.generateMeal(mealRequestDomainObject);
 
         MealEntity mealEntity = mealMapper.mapMealDOToMealEntity(mealDomainObject);
         mealRepository.save(mealEntity);
 
+        sendMealNotification(mealDomainObject);
+
         return mealDomainObject;
     }
 
+    @Override
     public List<MealDomainObject> getAllMeals() {
         List<MealEntity> mealEntities = mealRepository.findAll();
         return mealEntities.stream()
@@ -49,12 +58,14 @@ public class MealServiceImpl implements MealService {
                 .toList();
     }
 
+    @Override
     public MealDomainObject getMealById(final Long id) throws NoSuchElementException {
         Optional<MealEntity> mealEntity = mealRepository.findById(id);
         return mealEntity.map(mealMapper::mapMealEntityToMealDO)
                 .orElseThrow(() -> new NoSuchElementException(MEAL_NOT_FOUND));
     }
 
+    @Override
     public void updateMeal(Long id, MealDomainObject mealDomainObject) throws NoSuchElementException {
         checkMealExists(id);
         MealEntity updatedMealEntity = mealMapper.mapMealDOToMealEntity(mealDomainObject);
@@ -62,12 +73,27 @@ public class MealServiceImpl implements MealService {
         mealRepository.save(updatedMealEntity);
     }
 
+    @Override
     public void deleteMeal(Long id) {
         mealRepository.deleteById(id);
     }
 
+    @Override
     public void deleteAllMeals() {
         mealRepository.deleteAll();
+    }
+
+    @Override
+    public void sendMealNotification(MealDomainObject mealDomainObject) {
+        String topicArn = "arn:aws:sns:us-east-1:324037301453:MyTopic";
+        String message = "A new meal has been generated: " + mealDomainObject.getMealName();
+
+        PublishRequest request = PublishRequest.builder()
+                .topicArn(topicArn)
+                .message(message)
+                .build();
+
+        snsClient.publish(request);
     }
 
     private void checkMealExists(final Long id) throws NoSuchElementException {
